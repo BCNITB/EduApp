a0d16f959838f7c385ec4234e8c4ba46d7db7412
BCNITB <97254492+BCNITB@users.noreply.github.com>
2023-12-29 10:26:37 +0100

    Minor bugs fixed in Sonidos App

--- sounds/src/app/enums/images.ts
+++ sounds/src/app/enums/images.ts
@@ -2,7 +2,7 @@ export enum Images {
     "assets/img/animals/horse.png" = 1,
     "assets/img/animals/goat.png",
     "assets/img/animals/pig.png",
-    "assets/img/animals/rooster.pnng",
+    "assets/img/animals/rooster.png",
     "assets/img/animals/monkey.png",
     "assets/img/animals/dog.png",
     "assets/img/animals/snake.png",
